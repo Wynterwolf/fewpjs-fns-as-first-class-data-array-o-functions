e81e5f847254f094e6975f5fb62499b5194049ff
Samantha Jordan <shinyguns@gmail.com>
2021-03-22 16:36:00 -0500

    done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function wakeDog(name, breed){
+    return `Wake ${name} the ${breed}.`
+}
+
+function leashDog(name, breed){
+    return `Leash ${name} the ${breed}.`
+}
+
+function walkToPark(name, breed) {
+    return `Walk to the park with ${name} the ${breed}.`
+}
+
+function throwFrisbee(name, breed){
+    return `Throw the frisbee for ${name} the ${breed}.`
+}
+
+function walkHome(name, breed) {
+    return 
+}
+
+function unleashDog(name, breed){
+
+}
